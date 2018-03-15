14e9a6a972a17faf3a396e14522f5b3c17424d0d
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-03-15 18:54:12 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry'
+class Song
+  @@count = 0
+  @@artist_count = {}
+  @@artists = []
+  @@genre_count = {}
+  @@genres = []
+
+attr_accessor :name, :artist, :genre
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      if @@artist_count.keys.include?(artist)
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end
+  end
+  @@artist_count
+end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+      @@genres.each do |genre|
+        if @@genre_count.keys.include?(genre)
+          @@genre_count[genre] += 1
+        else
+          @@genre_count[genre] = 1
+        end
+    end
+    @@genre_count
+  end
+
+end
